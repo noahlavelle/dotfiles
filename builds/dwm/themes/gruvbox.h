c3291d4259e3e0e4f2006cb0901b01ca48716f51
noahlavelle <noah.lavelle@gmail.com>
2021-11-22 17:12:27 +0000

    add bar scripts and builds folder

---
+++ builds/dwm/themes/gruvbox.h
@@ -0,0 +1,11 @@
+static const char black[]          = "#282828";
+static const char white[]          = "#ebdbb2";
+static const char gray2[]          = "#3c3836";
+static const char gray3[]          = "#504945";
+static const char gray4[]          = "#665c54";
+static const char blue[]           = "#83a598";
+static const char green[]          = "#8ec07c";
+static const char red[]            = "#fb4934";
+static const char orange[]         = "#fe8019";
+static const char yellow[]         = "#fabd2f";
+static const char pink[]           = "#c678dd";
